ffc371ae5809e97c4b47eb06e5258d44bf893a3b
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2025-11-09 12:57:46 +0100

    correct the floating point error in the finance calculator script

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,4 +5,4 @@ monthly_savings = monthly_income - monthly_total_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are ${monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
