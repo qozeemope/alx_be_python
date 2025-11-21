c6f3a05579b34e3059f4fc7c874ceac9f41a8e6b
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2025-11-21 10:43:33 +0100

    shopping list task

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,41 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def display_shopping_list (shopping_list):
+    print(f"Here are the items in your shopping list: {shopping_list}")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item = input("Add an item: ")
+            shopping_list.append(item)
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            item = input("Remove an item: ")
+            if item not in shopping_list:
+                print("Item not in the list")
+            else:
+                shopping_list.remove(item)
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            display_shopping_list(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
